b6ca692b0d76950d436d73152310065b9a2d36c5
ianimbuga <imbugaian20@gmail.com>
2024-09-23 12:55:01 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'IAN LIDAVALIA';
+console.log(currentUser)
+const welcomeMessage="Welcome to Flatbook, " +currentUser+"!"
+const excitedWelcomeMessage=welcomeMessage.toUpperCase()
+const shortGreeting="Welcome, " +currentUser[0] +"!"
+
+
